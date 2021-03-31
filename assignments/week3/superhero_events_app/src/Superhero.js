dc151e51edc3fce25ec8723a1bac3d16ceed275e
https://github.com/bu-AprilW/fsw-110.git <april.william@bryanuniversity.edu>
2021-03-31 03:44:13 -0500

    commit

---
+++ assignments/week3/superhero_events_app/src/Superhero.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './index.css';
+
+function Superhero(props) {
+    return (
+        <div className="superheroInfo">
+            <h1 className="name1">Name: {props.list.name}</h1>
+            <h3 className="show2">Show: {props.list.show} </h3>
+            <h5 className="phrase">Catch Phrase: "{props.list.catchPhrase}" </h5>
+            <img className="heropics" src={props.list.imageName} alt="Superhero Images"/>
+        </div>
+    )
+}
+
+export default Superhero
